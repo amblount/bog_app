0025f8182704ca91ce099e24bf66c3834ee17d52
Alivia Blount <aliviablount@gmail.com>
2016-10-18 10:04:02 -0700

    index, show, new working

--- bog_app/app/controllers/creatures_controller.rb
+++ bog_app/app/controllers/creatures_controller.rb
@@ -1,4 +1,30 @@
 class CreaturesController < ApplicationController
   def index
+    @creatures = Creature.all
+  end
+
+  def show
+    @creature = Creature.find(params[:id])
+  end
+
+  def new
+    @creature = Creature.new
+  end
+
+  def create
+
+      # whitelist params and save them to a variable
+      creature_params = params.require(:creature).permit(:name, :description)
+
+      # create a new creature from `creature_params`
+      creature = Creature.new(creature_params)
+
+      # if creature saves, redirect to route that displays all creatures
+      if creature.save
+        redirect_to creature_path(creature)
+        # redirect_to creatures_path is equivalent to:
+        # redirect_to "/creatures"
+      end
+
   end
 end
